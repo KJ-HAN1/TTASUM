e794fb203da385d203bcee4a24021a7c64962b51
kimyelin0506 <annalin1004@gmail.com>
2025-05-19 17:29:03 +0900

    feat(TTASUM-86): 금칙어 AI 예외 처리

--- src/main/java/com/ttasum/memorial/exception/GlobalExceptionHandler.java
+++ src/main/java/com/ttasum/memorial/exception/GlobalExceptionHandler.java
@@ -1,16 +1,28 @@
 // 전역 예외 처리기
 package com.ttasum.memorial.exception;
 
+import com.ttasum.memorial.exception.forbiddenWord.ForbiddenWordException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.springframework.web.server.ResponseStatusException;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-    // ResponseStatusException 처리
-    @ExceptionHandler(ResponseStatusException.class)
-    public String handleStatusException(ResponseStatusException ex) {
-        return ex.getReason();
+    @ExceptionHandler(ForbiddenWordException.class)
+    public ResponseEntity<Map<String, Object>> handleForbiddenWordException(ForbiddenWordException e) {
+        Map<String, Object> errorResponse = new HashMap<>();
+        errorResponse.put("success", false);
+        errorResponse.put("code", 400);
+        errorResponse.put("forbiddenPoint", e.getForbiddenPoint());
+        errorResponse.put("message", e.getMessage());
+
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(errorResponse);
     }
 }
