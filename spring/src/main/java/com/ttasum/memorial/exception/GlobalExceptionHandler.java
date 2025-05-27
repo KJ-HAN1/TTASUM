adc002c017d9326dfc3528b49c138a5deb9ee1e1
kimyelin0506 <annalin1004@gmail.com>
2025-05-27 11:22:20 +0900

    refactor(TTASUM-86): 전역 예외 처리 수정

--- spring/src/main/java/com/ttasum/memorial/exception/GlobalExceptionHandler.java
+++ spring/src/main/java/com/ttasum/memorial/exception/GlobalExceptionHandler.java
@@ -1,11 +1,13 @@
 // 전역 예외 처리기
 package com.ttasum.memorial.exception;
 
+import com.ttasum.memorial.exception.blameText.BlamTextException;
 import com.ttasum.memorial.exception.forbiddenWord.ForbiddenWordException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -13,12 +15,18 @@ import java.util.Map;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(ForbiddenWordException.class)
-    public ResponseEntity<Map<String, Object>> handleForbiddenWordException(ForbiddenWordException e) {
+    // ResponseStatusException 처리
+    @ExceptionHandler(ResponseStatusException.class)
+    public String handleStatusException(ResponseStatusException ex) {
+        return ex.getReason();
+    }
+
+    // 비난 글, 금칙어 확인 예외 처리
+    @ExceptionHandler({ForbiddenWordException.class, BlamTextException.class})
+    public ResponseEntity<Map<String, Object>> handleForbiddenWordException(RuntimeException e) {
         Map<String, Object> errorResponse = new HashMap<>();
         errorResponse.put("success", false);
         errorResponse.put("code", 400);
-        errorResponse.put("forbiddenPoint", e.getForbiddenPoint());
         errorResponse.put("message", e.getMessage());
 
         return ResponseEntity
