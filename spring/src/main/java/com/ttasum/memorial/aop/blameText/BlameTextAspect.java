be2b84975b01de695ca5023adc65efaf9ca273a6
kimyelin0506 <annalin1004@gmail.com>
2025-05-27 11:23:21 +0900

    feat(TTASUM-86): AOP 기능 구현

--- spring/src/main/java/com/ttasum/memorial/aop/blameText/BlameTextAspect.java
+++ spring/src/main/java/com/ttasum/memorial/aop/blameText/BlameTextAspect.java
@@ -1,8 +1,10 @@
 package com.ttasum.memorial.aop.blameText;
 
+import com.ttasum.memorial.dto.blameText.BlameResponseDto;
 import com.ttasum.memorial.dto.forbiddenWord.ReviewRequestDto;
 import com.ttasum.memorial.exception.blameText.BlamTextException;
 import com.ttasum.memorial.service.blameText.BlameTextCheckerService;
+import com.ttasum.memorial.service.forbiddenWord.ForbiddenWordCheckerService;
 import lombok.RequiredArgsConstructor;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
@@ -17,12 +19,17 @@ public class BlameTextAspect {
 
     @Autowired
     private final BlameTextCheckerService checkerService;
+    @Autowired
+    private final ForbiddenWordCheckerService forbiddenWordCheckerService;
 
     @Around("@annotation(com.ttasum.memorial.annotation.blameText.CheckBlameText)")
     public Object checkBlameText(ProceedingJoinPoint joinPoint) throws Throwable {
         for (Object arg : joinPoint.getArgs()) {
             if (arg instanceof ReviewRequestDto req) {
-                if(checkerService.checkBlameText(req.getSentence()) == 1){
+                BlameResponseDto response = checkerService.analyzeAndSave(req.getSentence());
+
+                if(response.getLabel() == 1){
+//                    forbiddenWordCheckerService.containsForbiddenWord(req.getSentence());
                     throw new BlamTextException("비난하는 의도가 예상 되는 글 입니다.");
                 }
             }
--- spring/src/main/java/com/ttasum/memorial/aop/forbiddenWord/ForbiddenWordAspect.java
+++ spring/src/main/java/com/ttasum/memorial/aop/forbiddenWord/ForbiddenWordAspect.java
@@ -1,38 +1,38 @@
-// AOP를 사용해서 특정 서비스 메서드에 자동으로 검사 삽입
-package com.ttasum.memorial.aop.forbiddenWord;
-
-import com.ttasum.memorial.dto.forbiddenWord.ReviewRequestDto;
-import com.ttasum.memorial.exception.forbiddenWord.ForbiddenWordException;
-import com.ttasum.memorial.service.forbiddenWord.ForbiddenWordCheckerService;
-import lombok.RequiredArgsConstructor;
-import org.aspectj.lang.ProceedingJoinPoint;
-import org.aspectj.lang.annotation.Around;
-import org.aspectj.lang.annotation.Aspect;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
-
-
-@Aspect
-@Component
-@RequiredArgsConstructor
-public class ForbiddenWordAspect {
-
-    @Autowired
-    private final ForbiddenWordCheckerService checkerService;
-
-    // 이 어노테이션이 붙은 메서드만 API를 거침
-    @Around("@annotation(com.ttasum.memorial.annotation.forbiddenWord.CheckForbiddenWord)")
-    public Object checkForbiddenWord(ProceedingJoinPoint joinPoint) throws Throwable {
-
-        for (Object arg : joinPoint.getArgs()) {
-            // 임시 DTO로 테스트(필드에 reviewText 하나만 존재)
-            if (arg instanceof ReviewRequestDto req) {
-                if (checkerService.containsForbiddenWord(req.getSentence())) {
-                    throw new ForbiddenWordException("금칙어가 포함되어 있습니다.", "letterContents");
-                }
-            }
-        }
-
-        return joinPoint.proceed();
-    }
-}
+//// AOP를 사용해서 특정 서비스 메서드에 자동으로 검사 삽입
+//package com.ttasum.memorial.aop.forbiddenWord;
+//
+//import com.ttasum.memorial.dto.forbiddenWord.ReviewRequestDto;
+//import com.ttasum.memorial.exception.forbiddenWord.ForbiddenWordException;
+//import com.ttasum.memorial.service.forbiddenWord.ForbiddenWordCheckerService;
+//import lombok.RequiredArgsConstructor;
+//import org.aspectj.lang.ProceedingJoinPoint;
+//import org.aspectj.lang.annotation.Around;
+//import org.aspectj.lang.annotation.Aspect;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Component;
+//
+//
+//@Aspect
+//@Component
+//@RequiredArgsConstructor
+//public class ForbiddenWordAspect {
+//
+//    @Autowired
+//    private final ForbiddenWordCheckerService checkerService;
+//
+//    // 이 어노테이션이 붙은 메서드만 API를 거침
+//    @Around("@annotation(com.ttasum.memorial.annotation.forbiddenWord.CheckForbiddenWord)")
+//    public Object checkForbiddenWord(ProceedingJoinPoint joinPoint) throws Throwable {
+//
+//        for (Object arg : joinPoint.getArgs()) {
+//            // 임시 DTO로 테스트(필드에 reviewText 하나만 존재)
+//            if (arg instanceof ReviewRequestDto req) {
+//                if (checkerService.containsForbiddenWord(req.getSentence())) {
+//                    throw new ForbiddenWordException("금칙어가 포함되어 있습니다.");
+//                }
+//            }
+//        }
+//
+//        return joinPoint.proceed();
+//    }
+//}
