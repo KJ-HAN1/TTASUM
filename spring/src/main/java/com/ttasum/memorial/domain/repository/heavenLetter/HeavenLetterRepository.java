c390f7a12a335b71901d01853d6d9f35b46e6750
Yeji11 <yeaji0804@gmail.com>
2025-05-30 20:18:56 +0900

    feat(TTASUM-88:ìdom)ain 정의완ë

---
+++ spring/src/main/java/com/ttasum/memorial/domain/repository/heavenLetter/HeavenLetterRepository.java
@@ -0,0 +1,7 @@
+package com.ttasum.memorial.domain.repository.heavenLetter;
+
+import com.ttasum.memorial.domain.entity.heavenLetter.HeavenLetter;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface HeavenLetterRepository extends JpaRepository<HeavenLetter, Integer> {
+}
