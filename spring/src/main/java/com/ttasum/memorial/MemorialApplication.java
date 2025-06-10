c606077b6e307c0ba15ffcdbd486ffe8c688f6f3
kukjin <hkjin@kakao.com>
2025-06-10 18:22:34 +0900

    <refactor>(TTASUM-114): .war deployment setting

--- spring/src/main/java/com/ttasum/memorial/MemorialApplication.java
+++ spring/src/main/java/com/ttasum/memorial/MemorialApplication.java
@@ -1,13 +1,25 @@
 package com.ttasum.memorial;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class MemorialApplication {
+public class MemorialApplication extends SpringBootServletInitializer {
 
-    public static void main(String[] args) {
-        SpringApplication.run(MemorialApplication.class, args);
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(MemorialApplication.class);
     }
 
+    public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.configure()
+                .ignoreIfMissing()
+                .load();
+        System.setProperty("DB_PW", dotenv.get("DB_PW"));
+
+        SpringApplication.run(MemorialApplication.class, args);
+    }
 }
---
+++ spring/src/main/java/com/ttasum/memorial/config/WebConfig.java
@@ -0,0 +1,17 @@
+package com.ttasum.memorial.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                //80port , local react port
+                .allowedOrigins("http://localhost:5173", "http://koda2.elementsoft.biz:8080")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowCredentials(true);
+    }
+}
