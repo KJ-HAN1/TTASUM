8fc70e65572fa4d13ebae9a3d435cfbf64829b2c
18hailey <hailey9459@gmail.com>
2025-06-11 15:17:18 +0900

    feat(TTASUM-84): 사용자 입력 받을 ChatDTO 정의

---
+++ spring/src/main/java/com/ttasum/memorial/dto/chat/ChatDto.java
@@ -0,0 +1,12 @@
+package com.ttasum.memorial.dto.chat;
+
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class ChatDto {
+    private String question;
+}
