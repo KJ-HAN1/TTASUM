0e8d0cf689f7f9c84baf851ec06fd161737527db
18hailey <hailey9459@gmail.com>
2025-06-11 15:10:35 +0900

    feat(TTASUM-84): WebClient 기본 설정 추가
    - CORS 설정
    - FastAPI 호출용 WebClient 설정

--- spring/src/main/java/com/ttasum/memorial/config/WebClientConfig.java
+++ spring/src/main/java/com/ttasum/memorial/config/WebClientConfig.java
@@ -1,18 +1,28 @@
 package com.ttasum.memorial.config;
 
-import jdk.jfr.ContentType;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import reactor.netty.http.client.HttpClient;
 
 import java.time.Duration;
 
 @Configuration
-public class WebClientConfig {
+public class WebClientConfig implements WebMvcConfigurer {
+
+    // CORS 설정
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://127.0.0.1:5501") // TODO: 프론트엔드 서버 추가해야 함
+                .allowedMethods("POST", "OPTIONS")
+                .allowedHeaders("*")
+                .maxAge(3600); // 초
+    }
 
     // 최대 대기 시간 설정
     private final HttpClient httpClient = HttpClient.create()
@@ -21,7 +31,7 @@ public class WebClientConfig {
     @Bean
     public WebClient getWebClient() {
         return WebClient.builder()
-                .baseUrl("http://127.0.0.1:8000")
+                .baseUrl("http://127.0.0.1:8000") // FastAPI 서버 주소
                 .clientConnector(new ReactorClientHttpConnector(httpClient))
                 .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
                 .build();
