d7406a1c372551d3e0d2c02524c7b2d80c0e45f7
18hailey <hailey9459@gmail.com>
2025-06-16 16:11:20 +0900

    refactor(TTASUM-84): ChatController 역할 분리 및 주석 추가

--- spring/src/main/java/com/ttasum/memorial/controller/ChatController.java
+++ spring/src/main/java/com/ttasum/memorial/controller/ChatController.java
@@ -1,7 +1,9 @@
 package com.ttasum.memorial.controller;
 
-import com.ttasum.memorial.dto.ChatDto;
-import com.ttasum.memorial.service.ChatService;
+import com.ttasum.memorial.dto.chat.ChatApiResponse;
+import com.ttasum.memorial.dto.chat.ChatDto;
+import com.ttasum.memorial.service.chat.ChatService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
 @RestController
+@Slf4j
 public class ChatController {
 
     private final ChatService chatService;
@@ -18,14 +21,28 @@ public class ChatController {
         this.chatService = chatService;
     }
 
+    /**
+     * @apiNote LLM 모델을 사용하여 챗봇 응답을 받는 API 엔드포인트
+     * @param inputMsg 사용자 입력 메시지 DTO
+     * @return {@code Mono<ResponseEntity<ChatApiResponse>>} 챗봇 응답 DTO
+     */
     @PostMapping("/chat")
-    public Mono<ResponseEntity<String>> getChatResponse(@RequestBody ChatDto question) {
+    public Mono<ResponseEntity<ChatApiResponse>> getChatResponse(@RequestBody ChatDto inputMsg) {
         // 입력 유효성 검사: null, 빈 공백만 입력할 경우
-        if(question.getQuestion() == null || question.getQuestion().trim().isEmpty()) {
-            return Mono.just(
-                    ResponseEntity.status(HttpStatus.BAD_REQUEST).body("빈 공백만 입력할 수 없습니다. 좀 더 구체적으로 입력해 주세요.")
-            );
+        String question = inputMsg.getQuestion();
+        if (question == null || question.trim().isEmpty()) {
+            return getValidationCheckError(question);
         }
-        return chatService.getLLMResponse(question.getQuestion());
+        log.info("[INPUT_VALIDATION_SUCCESS] OK 입력 유효성 검사 성공: \"{}\"", question);
+        return chatService.getLLMResponse(inputMsg);
+    }
+
+    private static Mono<ResponseEntity<ChatApiResponse>> getValidationCheckError(String question) {
+        log.warn("[INPUT_VALIDATION_FAILED] ERROR 사용자 입력 유효성 검사 실패: \"{}\"", question);
+        return Mono.just(
+                ResponseEntity
+                        .status(HttpStatus.BAD_REQUEST)
+                        .body(ChatApiResponse.error(HttpStatus.BAD_REQUEST.value(), "빈 공백만 입력할 수 없습니다. 좀 더 구체적으로 입력해 주세요."))
+        );
     }
 }
