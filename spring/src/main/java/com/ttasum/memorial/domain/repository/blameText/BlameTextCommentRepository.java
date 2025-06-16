7fb9040dc311da6e9b1475eb3625981b67cbc76c
kimyelin0506 <annalin1004@gmail.com>
2025-06-16 19:31:34 +0900

    feat(TTASUM-112): SQL 작성

--- spring/src/main/java/com/ttasum/memorial/domain/repository/blameText/BlameTextCommentRepository.java
+++ spring/src/main/java/com/ttasum/memorial/domain/repository/blameText/BlameTextCommentRepository.java
@@ -1,14 +1,18 @@
 package com.ttasum.memorial.domain.repository.blameText;
 
 import com.ttasum.memorial.domain.entity.blameText.BlameTextComment;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.util.ArrayList;
 import java.util.Optional;
 
 public interface BlameTextCommentRepository extends JpaRepository<BlameTextComment, Integer> {
-    Optional<BlameTextComment> findByComment_CommentSeqAndDeleteFlag(Integer OriginSeq, Integer deleteFlag);
+    Optional<BlameTextComment> findByOriginSeqAndDeleteFlag(Integer OriginSeq, Integer deleteFlag);
 //    @Query("SELECT b FROM BlameTextComment b WHERE b.comment.commentSeq = :commentSeq AND b.deleteFlag = :deleteFlag")
 //    Optional<BlameTextComment> findByCommentSeqAndDeleteFlag(@Param("commentSeq") Integer commentSeq, @Param("deleteFlag") Integer deleteFlag);
 
+    Page<BlameTextComment> findBlameTextCommentsByLabel(int label, Pageable pageable);
 }
