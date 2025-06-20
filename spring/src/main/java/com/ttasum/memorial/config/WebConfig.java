7ece3e5ff4ff51baba59f4019fa612c0a85fe45f
kimyelin0506 <annalin1004@gmail.com>
2025-06-20 13:39:13 +0900

    feat(TTASUM-123): front local, server Origin add

--- spring/src/main/java/com/ttasum/memorial/config/WebConfig.java
+++ spring/src/main/java/com/ttasum/memorial/config/WebConfig.java
@@ -9,7 +9,8 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // 또는 특정 경로
-                .allowedOrigins("http://localhost:5173") // 프론트 주소
+                .allowedOrigins("http://localhost:5173", "https://koda.elementsoft.biz")
+                // 프론트 주소
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS") // 꼭 PATCH, OPTIONS 포함
                 .allowedHeaders("*")
                 .allowCredentials(true);
